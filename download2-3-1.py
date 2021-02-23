f80da166743272f6a0208087de8d25713fd808d7
junmojeong <jjmstars7@gmail.com>
2021-02-23 11:26:12 +0900

    add code

---
+++ download2-3-1.py
@@ -0,0 +1,7 @@
+import sys
+import io
+import urllib.request as dw
+
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
